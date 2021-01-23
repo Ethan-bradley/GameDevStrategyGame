9965c306fc31b136b202dadf281436a14d6bf48b
Ethan Bradley <ethadeva@gmail.com>
2021-01-23 11:20:34 -0800

    Updated database

--- WOM/settings.py
+++ WOM/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': "foo",
+        'PASSWORD': "bar",
+        'HOST':"localhost",
+        "PORT": "5432",
     }
 }
 
